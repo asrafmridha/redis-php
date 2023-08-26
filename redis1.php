2229a34a9e03c878c4291b5d53de333acb528f6b
asraf-mridha <asrafmridha@gmail.com>
2023-08-26 15:07:59 +0600

    Redis expire time Set, array set and get with json_encode and json_decode

--- redis1.php
+++ redis1.php
@@ -8,6 +8,18 @@ $redis = new Client();
 //redis set key value
 $redis->set("name", "Tahmina Toma");
 
+// redis expire time with second 
+$redis->expire("name", 30);
+
+//redis set array with json_encode ...without json_encode it's not possible
+$redis->set('user1',json_encode([
+    "name"  => "Tahmina Toma",
+    "email" => "tahminatoma@gmail.com"
+]));
+
+// to see with associative array 
+print_r(json_decode($redis->get("user1"),true));
+
 //redis get key value
 echo $redis->get("name");
 
