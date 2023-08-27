7ff4cfb440481bb26c43174707ad2c320c6d0b9d
asraf-mridha <asrafmridha@gmail.com>
2023-08-27 12:06:34 +0600

    hases and array set and get in redis

---
+++ redis3.php
@@ -0,0 +1,28 @@
+<?php
+
+require "vendor/autoload.php";
+$redis = new Predis\Client();
+
+$identity = [
+    'name'      => 'Tahmina Toma',
+    'address'   => "Char Afra",
+    'husband'   => 'N/A',
+];
+
+// to set one by one
+
+/* $redis->hset('identity01',  'name',     'Tahmina Toma');
+$redis->hset('identity01',  'address',  'Char Afra');
+$redis->hset('identity01',  'husband',  'N/A'); */
+
+//to set all
+$redis->hmset('identity01', $identity);
+
+// to see one by one 
+/* 
+echo $redis->hget('identity01','name');
+echo $redis->hget('identity01', 'address');
+echo $redis->hget('identity01', 'husband');  */
+
+//to see all
+$redis->hmget('identity01');
